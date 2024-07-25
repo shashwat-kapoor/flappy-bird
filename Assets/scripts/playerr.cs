dcb6a27f0f72330f20c90f722164446ca4d37f9e
shashwat kapoor <147691001+shashwat-kapoor@users.noreply.github.com>
2024-07-25 06:15:44 +0530

    Update playerr.cs

--- Assets/scripts/playerr.cs
+++ Assets/scripts/playerr.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 public class playerr : MonoBehaviour
 {
     private Vector3 direction;
-    public float gravity = -9.8;
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+    }
+}
 
 
 }
